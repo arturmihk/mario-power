5aac3242ac82c3672246fd57159c3b898eb1b56c
arturmihk <115464760+arturmihk@users.noreply.github.com>
2022-12-14 08:49:06 +0200

    Update Powershell_too.ps1

--- Powershell_too.ps1
+++ Powershell_too.ps1
@@ -11,15 +11,17 @@ $d = Split-path $spath
 $fail = Read-Host -prompt ("Sisesta csv faili nimi")
 if ($fail -like '*.csv*') {$kasutajad = Import-Csv $d\$fail}
 else {Write-Host "Pole .csv"}
+#loop
 foreach ($kasutaja in $kasutajad)
 {
+#nimede splittimine
 $email = $kasutaja.emails
 $enimi = $email.split(".")[0]
 $ppnimi = $email.split("@")[0]
 $pnimi = $ppnimi.split(".")[1]
-
+#suure tähe tegemine
 $oenimi = $enimi.substring(0,1).ToUpper()+$enimi.substring(1).tolower()
 $opnimi = $pnimi.substring(0,1).ToUpper()+$pnimi.substring(1).tolower()
-
+#faili copimine
 $oenimi+" "+$opnimi >> $d\nimed.txt
 }
